8886ae3076d6721c504bb955f929059e86dbf39d
ShanviSingla <65721111+ShanviSingla@users.noreply.github.com>
2020-06-14 22:11:31 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -39,11 +39,12 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(255);
  
+  groundObject.display();
   dustbinObj.display();
   paperObject.display();
-  groundObject.display();
+  
   
  
   
@@ -54,7 +55,7 @@ function draw() {
 function keyPressed() {
   	if (keyCode === UP_ARROW) {
 
-    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:90,y:-90});
     
   	}
 }
